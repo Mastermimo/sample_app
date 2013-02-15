996c382a23ce4d5c602712242c63b5821435dd96
Mimo <mrmimo7@gmail.com>
2013-02-14 23:00:30 -0500

    Add SSL in production

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -31,6 +31,13 @@ describe "User pages" do
       end
     end
 
+    describe "after submission" do
+      before { click_button submit }
+
+      it { should have_selector('title', text: 'Sign up') }
+      it { should have_content('error') }
+    end
+
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
@@ -43,6 +50,14 @@ describe "User pages" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
     end
+  end
+
+  describe "after saving the user" do
+    before { click_button submit }
+    let(:user) { User.find_by_email('user@example.com') }
+
+    it { should have_selector('title', text: user.name) }
+    it { should have_selector('div.alert.alert-success', text: 'Welcome') }
   end
 
 end
